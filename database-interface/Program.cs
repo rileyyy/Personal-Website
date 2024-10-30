4751852557754e3cbec59224b8ad5e60e80a5261
Riley Schroeder <rileyyy@live.com>
2024-10-30 21:39:20 +0100

    Fix mongodb secrets and get asp talking to it

--- database-interface/Program.cs
+++ database-interface/Program.cs
@@ -1,7 +1,25 @@
+using MongoDB.Driver;
+
 var builder = WebApplication.CreateBuilder(args);
 
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+var username = string.Empty;
+var password = string.Empty;
+File.ReadAllLines("/run/secrets/mongodb_username")
+    .ToList()
+    .ForEach(line => username = line );
+File.ReadAllLines("/run/secrets/mongodb_password")
+    .ToList()
+    .ForEach(line => password = line );
+
+var connectionString = $"mongodb://{username}:{password}@mongodb:27017";
+
+var client = new MongoClient(connectionString);
+
+app.MapGet("/", () => {
+  return $"Connection string: {connectionString}\n" +
+         $"Database names: {string.Join(", ", client.ListDatabaseNames().ToList())}";
+});
 
 app.Run("http://*:25052/");
