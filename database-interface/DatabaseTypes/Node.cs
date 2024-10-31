a24e39e3aa25621f14451b10984567f246b2f0c1
Riley Schroeder <rileyyy@live.com>
2024-10-31 22:41:30 +0100

    Update nodes type and fix some naming inconsistencies

--- database-interface/DatabaseTypes/Node.cs
+++ database-interface/DatabaseTypes/Node.cs
@@ -1,32 +1,57 @@
 using System.Collections.Generic;
+using System.Linq;
 using MongoDB.Bson;
 using MongoDB.Bson.Serialization.Attributes;
 
 namespace database_interface.DatabaseTypes;
 
-public struct Node
+public class Node
 {
   [BsonId]
   [BsonElement("_id")]
-  [BsonRepresentation(BsonType.ObjectId)]
-
-  public string Id { get; set; }
+  public ObjectId Id { get; set; }
 
   [BsonElement("name")]
-  public string Name { get; set; }
+  public required string Name { get; set; }
 
   [BsonElement("icon")]
-  public string Icon { get; set; }
+  public required string Icon { get; set; }
 
   [BsonElement("position")]
-  public int[] Position { get; set; }
+  public required int[] Position { get; set; }
 
-  [BsonElement("parentNodeId")]
-  public string ParentNodeId { get; set; }
+  [BsonElement("parentNode")]
+  public string? ParentNode { get; set; }
 
-  [BsonElement("shownNodeIds")]
-  public List<string> ShownNodeIds { get; set; }
+  [BsonElement("showNodes")]
+  public required List<string> ShowNodes { get; set; }
 
   [BsonElement("nodeType")]
   public NodeType NodeType { get; set; }
+
+  public override bool Equals(object? obj)
+  {
+    if (obj is Node otherNode)
+    {
+      return Name == otherNode.Name &&
+             Icon == otherNode.Icon &&
+             Position.SequenceEqual(otherNode.Position) &&
+             ParentNode == otherNode.ParentNode &&
+             ShowNodes.SequenceEqual(otherNode.ShowNodes) &&
+             NodeType == otherNode.NodeType;
+    }
+    return false;
+  }
+
+  public override int GetHashCode()
+  {
+    int hash = 17;
+    hash = hash * 23 + (Name?.GetHashCode() ?? 0);
+    hash = hash * 23 + (Icon?.GetHashCode() ?? 0);
+    hash = hash * 23 + (Position?.GetHashCode() ?? 0);
+    hash = hash * 23 + (ParentNode?.GetHashCode() ?? 0);
+    hash = hash * 23 + (ShowNodes?.GetHashCode() ?? 0);
+    hash = hash * 23 + NodeType.GetHashCode();
+    return hash;
+  }
 }
