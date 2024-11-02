e234f129cbb00bb19090cf33f3289e6d6870f0e7
Riley Schroeder <rileyyy@live.com>
2024-11-02 15:32:35 +0100

    Remove testing nodes and do some light formatting

--- website/src/App.vue
+++ website/src/App.vue
@@ -5,51 +5,9 @@ import TransitionEdge from './components/TransitionEdge.vue'
 
 const { onInit } = useVueFlow()
 
-const nodes = ref([
-  {
-    id: '1',
-    data: { 
-      label: 'DblClick me',
-      showNodes: ['1', '2', '3'],
-      mainHub: true,
-     },
-    position: { x: 0, y: 0 },
-  },
-  {
-    id: '2',
-    type: 'output',
-    data: { 
-      label: 'DblClick me',
-      showNodes: ['1', '2'],
-    },
-    position: { x: 300, y: 300 },
-    targetPosition: Position.Left,
-  },
-  {
-    id: '3',
-    data: { 
-      label: 'DblClick me',
-      showNodes: ['1', '3', '4'],
-     },
-    position: { x: -300, y: -300 },
-  },
-  {
-    id: '4',
-    type: 'output',
-    data: { 
-      label: 'DblClick me',
-      showNodes: ['3', '4'],
-    },
-    position: { x: -600, y: -600 },
-    targetPosition: Position.Right,
-  },
-])
+const nodes = ref([])
+const edges = ref([])
 
-const edges = ref([
-  { id: 'e1-2', type: 'custom', source: '1', target: '2', style: { stroke: '#fff' } },
-  { id: 'e1-3', type: 'custom', source: '1', target: '3', style: { stroke: '#fff' } },
-  { id: 'e3-4', type: 'custom', source: '3', target: '4', style: { stroke: '#fff' } },
-])
 
 onInit(({ fitView }) => {
   fitView({ nodes: ['1', '2', '3'] })
