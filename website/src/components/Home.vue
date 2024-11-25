21cd689262b08a98e6512a00a15b9e6f4979115d
Riley Schroeder <rileyyy@live.com>
2024-11-25 17:26:55 +0100

    Add ProjectNode rendering

--- website/src/components/Home.vue
+++ website/src/components/Home.vue
@@ -5,12 +5,52 @@ import { fetchDataAsync } from '../infrastructure/DatabaseService.ts'
 import TransitionEdge from './TransitionEdge.vue'
 import NodeOnly from './Nodes/NodeOnly.vue'
 import Header from './Header.vue'
+import ProjectNode from './Nodes/ProjectNode.vue'
 
 const { fitView } = useVueFlow()
 
-const edges = ref([])
 
-function parseNodes(data) {
+function renderStartingNodes() {
+  let homeNode = nodes.value.find((node) => node.id === 'Home');
+
+  setTimeout(() => fitView({
+    nodes: homeNode.data.showNodes,
+    duration: 1000,
+  }));
+
+  homeNode.data.showNodes.forEach((node) => {
+    nodes.value.find((n) => n.id === node).hidden = false;
+  });
+}
+
+onBeforeMount(async () => {
+  fetchDataAsync('nodes')
+    .then((response) => parseNodes(response))
+    .then(() => setTimeout(renderStartingNodes, 1000))
+    .then(() => setTimeout(calculateEdges, 1750))
+});
+</script>
+
+<script>
+export const nodes = ref([]);
+export const edges = ref([])
+
+export function setNodesVisible(showNodes) {
+  let nodesArr = Array.isArray(showNodes)
+                  ? showNodes
+                  : JSON.parse(showNodes);
+
+  nodesArr.forEach((node) => {
+    if (node === null)
+      return;
+
+    let n = nodes.value.find((n) => n.id === node);
+    if (n.hidden)
+      n.hidden = false;
+  });
+}
+
+export function parseNodes(data) {
   data.forEach((node) => {
     nodes.value.push({
       id: node.name,
@@ -28,20 +68,7 @@ function parseNodes(data) {
   });
 }
 
-function renderStartingNodes() {
-  let homeNode = nodes.value.find((node) => node.id === 'Home');
-
-  setTimeout(() => fitView({
-    nodes: homeNode.data.showNodes,
-    duration: 1000,
-  }));
-
-  homeNode.data.showNodes.forEach((node) => {
-    nodes.value.find((n) => n.id === node).hidden = false;
-  });
-}
-
-function calculateEdges() {
+export function calculateEdges() {
   nodes.value.forEach((node) => {
     if (Array.isArray(node.data.showNodes)) {
       node.data.showNodes.forEach((showNode) => {
@@ -59,31 +86,6 @@ function calculateEdges() {
   });
 }
 
-onBeforeMount(async () => {
-  fetchDataAsync('nodes')
-    .then((response) => parseNodes(response))
-    .then(() => setTimeout(renderStartingNodes, 1000))
-    .then(() => setTimeout(calculateEdges, 1750))
-});
-</script>
-
-<script>
-export const nodes = ref([]);
-
-export function setNodesVisible(showNodes) {
-  let nodesArr = Array.isArray(showNodes)
-                  ? showNodes
-                  : JSON.parse(showNodes);
-
-  nodesArr.forEach((node) => {
-    if (node === null)
-      return;
-
-    let n = nodes.value.find((n) => n.id === node);
-    if (n.hidden)
-      n.hidden = false;
-  });
-}
 export function updateNodeCollection(nodeId) {
   let node = nodes.value.find((n) => n.id === nodeId);
 
@@ -116,6 +118,37 @@ export function updateNodeCollection(nodeId) {
   });
 }
 
+
+function getNodeTypeFromParent(parentNode) {
+  switch(parentNode.id) {
+    case 'Journal':   return 'JournalNode';
+    case 'Projects':  return 'ProjectNode';
+    default:          return 'NodeOnly';
+
+  }
+}
+
+function addNodeSpecificData(newNode, entry) {
+  switch(newNode.type) {
+    case 'JournalNode':
+      newNode.data.id = entry.id;
+      newNode.data.title = entry.title;
+      newNode.data.tags = entry.tags;
+      newNode.data.listDate = entry.listDate;
+      newNode.data.content = entry.content;
+      break;
+
+    case 'ProjectNode':
+      newNode.data.description = entry.description;
+      newNode.data.link = entry.link;
+      newNode.data.technologies = entry.technologies;
+      newNode.data.images = entry.images;
+      break;
+
+    default:
+      break;
+  }
+}
 </script>
 
 <template>
@@ -125,7 +158,7 @@ export function updateNodeCollection(nodeId) {
     v-model:edges="edges"
     class="transition-flow"
     :fit-view-on-init="true"
-    :node-types="{NodeOnly: NodeOnly}">
+    :node-types="{NodeOnly: NodeOnly, ProjectNode: ProjectNode}">
 
     <template #edge-custom="props">
       <TransitionEdge v-bind="props" />
@@ -133,6 +166,10 @@ export function updateNodeCollection(nodeId) {
 
     <template #nodeOnly="props">
       <NodeOnly :id="props.id" :data="props.data"/>
+    </template>
+
+    <template #projectNode="props">
+      <ProjectNode :id="props.id" :data="props.data"/>
     </template>
   </VueFlow>
 </template>
---
+++ website/src/components/Nodes/ProjectNode.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="project-node">
+    <h1>{{ id }}</h1>
+  </div>
+</template>
+
+<script setup>
+import { Handle, Position } from '@vue-flow/core'
+
+const props = defineProps({
+  id: {
+    type: String,
+    required: true,
+  },
+  data: {
+    type: Object,
+    required: true,
+  },
+})
+
+</script>
+
+<style>
+  .project-node {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    width: 300px;
+    height: 100px;
+    background-color: var(--charcoal);
+    border-radius: 50px;
+    color: var(--cadet-gray);
+    position: relative;
+    transition: box-shadow 1s ease;
+  }
+</style>
